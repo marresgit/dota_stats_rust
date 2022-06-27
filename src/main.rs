2c23fc923a12bb182cf24f53b186b5341bbcb467
Mauricio Saez <Mauricio.saez@tillvaxtverket.se>
2022-06-27 09:03:16 +0200

    Working with reqwest & dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+
+use dotenv::dotenv;
+use reqwest::header::{HeaderMap, AUTHORIZATION};
+use std::collections::HashMap;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let api_key = env::var("API_KEY")?;
+
+    let mut headers = HeaderMap::new();
+
+    headers.insert(AUTHORIZATION, format!("Bearer {}",api_key).parse().unwrap());
+
+    let client = reqwest::Client::new();
+    let resp = client
+        .get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
